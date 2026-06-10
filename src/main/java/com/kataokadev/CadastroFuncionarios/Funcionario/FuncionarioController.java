f05b803f4dd5e1a548c9406e3c49b91ac8523fd5
Rafael Kataoka <rafaelktk2005@gmail.com>
2026-06-10 19:25:08 -0300

    feat: Atualiza controller com mock de dados

--- src/main/java/com/kataokadev/CadastroFuncionarios/Funcionario/FuncionarioController.java
+++ src/main/java/com/kataokadev/CadastroFuncionarios/Funcionario/FuncionarioController.java
@@ -6,8 +6,33 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/rota")
 public class FuncionarioController {
 
-    @GetMapping
-    public String boasVindas(){
-        return "Seja Bem Vindo a minha rota";
+    // Adicionar Funcionario (CREATE)
+    @PostMapping("criar")
+    public String criarFuncionario() {
+        return "Funcionario criado com sucesso!";
+    }
+
+    // Mostrar todos os Funcionarios (READ)
+    @GetMapping("/todos")
+    public String todosFuncionarios() {
+        return "Funcionarios todos";
+    }
+
+    // Procurar Funcionario por id (CREATE)
+    @GetMapping("todosID")
+    public String todosFuncionariosID() {
+        return "Funcionarios ID";
+    }
+
+    // Alterar dados de Funcionario (UPDATE)
+    @PutMapping("alterarID")
+    public String alterarFuncionarioID() {
+        return "Funcionario alterado com sucesso!";
+    }
+
+    // Deletar Funcionario (DELETE)
+    @DeleteMapping("deletarID")
+    public String deletarFuncionarioID() {
+        return "Funcionario deletado com sucesso!";
     }
 }
