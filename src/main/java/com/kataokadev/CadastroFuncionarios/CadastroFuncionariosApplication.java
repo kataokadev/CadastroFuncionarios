5b0e8837f72765fb6f3abfcfabf9e267df2d334e
Rafael Kataoka <rafaelktk2005@gmail.com>
2026-06-09 21:47:02 -0300

    Buildando Spring

---
+++ src/main/java/com/kataokadev/CadastroFuncionarios/CadastroFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package com.kataokadev.CadastroFuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroFuncionariosApplication.class, args);
+	}
+
+}
