26ddd3000af3c2e9d3824677ba92f2d95a46a061
Rafael Kataoka <rafaelktk2005@gmail.com>
2026-06-09 22:58:13 -0300

    Cria uma entidade no banco de dados apartir do FuncionarioModel

---
+++ src/main/java/com/kataokadev/CadastroFuncionarios/FuncionarioModel.java
@@ -0,0 +1,64 @@
+package com.kataokadev.CadastroFuncionarios;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastro")
+public class FuncionarioModel {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String cargo;
+
+    @Column(unique = true)
+    private String email;
+
+    public FuncionarioModel() {
+    }
+
+    public FuncionarioModel(String nome, String cargo, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
