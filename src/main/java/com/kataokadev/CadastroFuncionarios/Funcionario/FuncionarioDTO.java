0bce427e280282f62fa82fb9312517d57a1e3ea0
Rafael Kataoka <rafaelktk2005@gmail.com>
2026-06-11 02:59:47 -0300

    chore: Inclui a versao do DTO pela entidade e mapeia manualmente pelo Mapper os caminhos Entidade x DTO

--- src/main/java/com/kataokadev/CadastroFuncionarios/Funcionario/FuncionarioDTO.java
+++ src/main/java/com/kataokadev/CadastroFuncionarios/Funcionario/FuncionarioDTO.java
@@ -1,4 +1,21 @@
 package com.kataokadev.CadastroFuncionarios.Funcionario;
 
+import com.kataokadev.CadastroFuncionarios.Projeto.ProjetoModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FuncionarioDTO {
+
+    private Long id;
+    private String nome;
+    private String cargo;
+    private String email;
+    private String cpf;
+    private ProjetoModel projeto;
+
 }
